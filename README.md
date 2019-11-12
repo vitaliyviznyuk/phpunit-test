bc6c3850ff0ef9835f8aec82054ed4b9243d8d96
Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
2019-11-12 19:48:27 +0200

    Implement example_2.1

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # phpunit-test
 
 Examples from PHPUnit Manual (https://phpunit.readthedocs.io/en/8.4/index.html).
- 
---
+++ src/chapter_2/example_2.1/index.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/**
+ * @author Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
+ * @copyright Copyright (c) 2019 Vitaliy Viznyuk
+ */
+
+namespace vitaliyviznyuk\PHPUnitTest;
+
+use PHPUnit\Framework\TestCase;
+
+class StackTest extends TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertSame(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertSame(1, count($stack));
+
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertSame(0, count($stack));
+    }
+}
