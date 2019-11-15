2355f0638bb0df0529f39c840f32b95de1b9c2ef
Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
2019-11-15 23:40:57 +0200

    Implement example_2.5

---
+++ src/chapter_2/example_2.5/index.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+/**
+ * @author Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
+ * @copyright Copyright (c) 2019 Vitaliy Viznyuk
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class DataTest extends TestCase
+{
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testAdd($a, $b, $expected)
+    {
+        $this->assertSame($expected, $a + $b);
+    }
+
+    public function additionProvider()
+    {
+        return [
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 3]
+        ];
+    }
+}
