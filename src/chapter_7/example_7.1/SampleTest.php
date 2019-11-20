a95ce9e4b6a999b048ad91a5867ac359bf39e451
Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
2019-11-20 23:59:03 +0200

    Implement example_7.1

---
+++ src/chapter_7/example_7.1/SampleTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+/**
+ * @author Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
+ * @copyright Copyright (c) 2019 Vitaliy Viznyuk
+ */
+
+namespace vitaliyviznyuk\PHPUnitTest;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testSomething()
+    {
+        // Optional: Test anything here, if you want.
+        $this->assertTrue(true, 'This should already work.');
+
+        // Stop here and mark this test as incomplete.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
