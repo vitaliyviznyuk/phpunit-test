ebf236a25c12a1790b85e9dd3d8678675e77ed31
Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
2019-11-25 22:29:45 +0200

    Implement example_8.1

---
+++ src/chapter_8/example_8.1/SomeClass.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * @author Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
+ * @copyright Copyright (c) 2019 Vitaliy Viznyuk
+ */
+
+namespace vitaliyviznyuk\PHPUnitTest;
+
+class SomeClass
+{
+    public function doSomething()
+    {
+        // Do something.
+    }
+}
