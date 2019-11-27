3068852830629720b38088f3301db3c5a613bc56
Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
2019-11-27 23:58:54 +0200

    Implement example_8.2

---
+++ src/chapter_8/example_8.2/StubTest.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+/**
+ * @author Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
+ * @copyright Copyright (c) 2019 Vitaliy Viznyuk
+ */
+
+namespace vitaliyviznyuk\PHPUnitTest;
+
+require __DIR__ . '/../example_8.1/SomeClass.php';
+
+use PHPUnit\Framework\TestCase;
+
+class StubTest extends TestCase
+{
+    public function testStub()
+    {
+        // Create a stub for the SomeClass class.
+        $stub = $this->createStub(SomeClass::class);
+
+        // Configure the stub.
+        $stub->method('doSomething')
+             ->willReturn('foo');
+
+        // Calling $stub->doSomething() will now return
+        // 'foo'.
+        $this->assertSame('foo', $stub->doSomething());
+    }
+}
