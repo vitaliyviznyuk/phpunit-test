458f2914489a4259b754e4f39759a6065b72f34a
Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
2019-12-01 22:00:51 +0200

    Implement example_8.14

---
+++ src/chapter_8/example_8.14/FooTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+/**
+ * @author Vitaliy Viznyuk <vitaliyviznyuk@gmail.com>
+ * @copyright Copyright (c) 2019 Vitaliy Viznyuk
+ */
+
+namespace vitaliyviznyuk\PHPUnitTest;
+
+require __DIR__ . '/../example_8.11/Subject.php';
+
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class FooTest extends TestCase
+{
+    public function testFunctionCalledTwoTimesWithSpecificArguments()
+    {
+        $mock = $this->getMockBuilder(stdClass::class)
+                     ->setMethods(['set'])
+                     ->getMock();
+
+        $mock->expects($this->exactly(2))
+             ->method('set')
+             ->withConsecutive(
+                 [$this->equalTo('foo'), $this->greaterThan(0)],
+                 [$this->equalTo('bar'), $this->greaterThan(0)]
+             );
+
+        $mock->set('foo', 21);
+        $mock->set('bar', 48);
+    }
+}
